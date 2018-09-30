1a0f447e33a2a1a4f72a3d1b236b71d5c3c0b258
elatsoshvili <samurai.girl89@gmail.com>
2018-09-30 14:24:45 +0400

    Update root.js

--- app/routes/root.js
+++ app/routes/root.js
@@ -1,5 +1,5 @@
 module.exports = function(req, res, next) {
   res.contentType = "json";
-  res.send({ message: "Hello Devops Georgia Day 2" });
+  res.send({ message: "Hello Devops Georgia Day 2 Hi" });
   next();
 };
